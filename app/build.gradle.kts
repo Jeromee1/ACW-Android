f152a66bbabc4f4f865305ad2a776e83eb82e160
JeromeE <jeremyho885@gmail.com>
2026-01-20 11:09:56 +0800

    Minor extra stuff

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.apa.alumnidirectory"
+        applicationId = "com.jeremy.acw"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
